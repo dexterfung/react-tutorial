808f3085853f446b72bde5cd760977fdd75a29ed
Dexter Fung <dexter@dexterfung.com>
2019-01-07 23:53:44 +0800

    Finish states

--- src/App.js
+++ src/App.js
@@ -2,10 +2,46 @@ import React, {Component} from 'react';
 import Table from './Table'
 
 class App extends Component {
+    state = {
+        characters: [
+            {
+                'name': 'DenDen',
+                'job': 'Dog'
+            },
+            {
+                'name': 'Mund',
+                'job': 'IT Dog'
+            },
+            {
+                'name': 'Bob',
+                'job': 'IT Dog'
+            }, 
+            {
+                'name': 'Bill',
+                'job': 'Dog'
+            }
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((characters, i) => {
+                return i !== index;
+            })
+        })
+    }
+
     render () {
+        const { characters } = this.state;
+        
         return (
             <div className="container">
-                <Table />
+                <Table 
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
             </div>
         );
     }
