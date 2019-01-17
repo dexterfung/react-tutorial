75c8bc3e6f7e7261836fd6ff3a7d6e07a08f7bf4
Dexter Fung <dexter@dexterfung.com>
2019-01-17 11:01:40 +0000

    Updates

---
+++ precache-manifest.688eab896ae672181342f8b4388fe8e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "a88b7bb08bd4adac865b",
+    "url": "/react-tutorial/static/js/main.a88b7bb0.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "a88b7bb08bd4adac865b",
+    "url": "/react-tutorial/static/css/main.47674c18.chunk.css"
+  },
+  {
+    "revision": "4968770495c4f1796679ee40cf1df5df",
+    "url": "/react-tutorial/index.html"
+  }
+];
